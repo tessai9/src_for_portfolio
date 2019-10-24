de5433a9d14d9b25b583a1d943ff8eda7892ee1c
tessai9 <lon1ago9@yahoo.co.jp>
2019-10-24 23:04:08 +0900

    add works, modified profile

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -3,8 +3,9 @@
     <div id="works" class="topics">
       <h1 class="section_title">Works</h1>
       <ion-icon name="logo-github" class="icon" v-b-tooltip.hover title="Github" v-on:click="openUrl('https://github.com/tessai9')"></ion-icon>
-      <ion-icon name="create" class="icon" v-b-tooltip.hover title="Blog" v-on:click="openUrl('http://blog.livedoor.jp/tessai99/')"></ion-icon>
+      <ion-icon name="create" class="icon" v-b-tooltip.hover title="Blog" v-on:click="openUrl('http://tesao.work/')"></ion-icon>
       <ion-icon name="people" class="icon" v-b-tooltip.hover title="三宮.dev" v-on:click="openUrl('https://kobe-sannomiya-dev.connpass.com/')"></ion-icon>
+      <ion-icon name="megaphone" class="icon" v-b-tooltip.hover title="webサービス：有言実GO" v-on:click="openUrl('https://www.yugenjigo.work/')"></ion-icon>
     </div>
   </section>
 </template>
